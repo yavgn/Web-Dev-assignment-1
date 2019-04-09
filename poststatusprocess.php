96f3a1c573077e50fd7883ce6003b132b9c255f5
yavgn <42362192+yavgn@users.noreply.github.com>
2019-04-10 11:57:34 +1200

    Update poststatusprocess.php
    check for alphanumeric and table exists

--- poststatusprocess.php
+++ poststatusprocess.php
@@ -15,24 +15,17 @@
 	
 		require_once ("/home/yfd0036/conf/settings.php");
 
-		$checkQuery = "Select Status_ID from yfd0036.Status where Status_ID = '$code'";
+		$checkCode = "Select Status_ID from yfd0036.Status where Status_ID = '$code'";
 		$checkTable = "Show tables where Tables_in_yfd0036 = 'Status'";
-		$createTable = "Create table Status(Status_ID varchar(5) not null, Status varchar varchar(100) not null, Share_Status varchar(10) not null, Date date format 'dd/mm/yyyy' not null, Permissions varchar(40), Primary Key (Status_ID))";
+		$createTable = "Create table if not exists Status(Status_ID varchar(5) not null, Status varchar(100) not null, Share_Status varchar(10) not null, Date date not null, Permissions varchar(40), Primary Key (Status_ID))";
 
 		//set up connection
 		$dbConnect = @mysqli_connect($host, $user, $pswd, $dbnm);
 		if (!$dbConnect){
 			die("<p>The database server is not available.</p>");
 		}
-
-		function tableExists(){
-			if(!mysqli_query($dbConnect, $checkTable)){
-				mysqli_query($dbConnect, $createTable);
-				echo "<p>Table Created</p>";
-			}
-		}
-
-		tableExists();
+		//create table if it doesnt exist
+		mysqli_query($dbConnect, $createTable);
 
 		//checks for missing fields, pattern of status code and pattern of status
 		if(empty($code) || empty($status) || empty($shareStatus) || empty($date) || preg_match('/^S\d{4}$/', $code) == false || preg_match('/^[a-zA-Z0-9,.!? ]*$/', $status) == false){
@@ -57,14 +50,13 @@
 			//query to insert into
 			$query = "insert into yfd0036.Status"."(Status_ID, Status, Share_Status, Date, Permissions)"."values"."('$code','$status','$shareStatus','$date','$perms')";
 			if (mysqli_query($dbConnect, $query)){
-				echo "<p>Record successfully inserted </p>";
-
 				echo "<p>Code: " . $code  . "</p>" . "<p>Status: " . $status  . "</p>" .
 				"<p>Share State: " . $shareStatus  . "</p>" . "<p>Date: " . $date  . "</p>" . 
 				"<p>Permissions: " . $perms  . "</p>";
 			}
 			else{
-				$result = mysqli_query($dbConnect, $checkQuery);
+				//checks if status code exist
+				$result = mysqli_query($dbConnect, $checkCode);
 				if (mysqli_num_rows($result) > 0) {
 					echo "<p>Code already exists</p>";
 				}
@@ -72,8 +64,8 @@
 					echo "<p>Error, something is wrong with the query</p>";
 				}
 			}	
-			mysqli_close($dbConnect);
 		}
+		mysqli_close($dbConnect);
 
 	?>
 	<a href="poststatusform.php">Post another status</a>
